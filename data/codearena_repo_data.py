59c7291402f2e83d2762ed7861ba203d4b78103d
Ronit Pattanayak <rpatt@umich.edu>
2025-02-01 15:11:16 -0500

    Open Interpreter PRs and Tasks

--- data/codearena_repo_data.py
+++ data/codearena_repo_data.py
@@ -1,17 +1,14 @@
 {
-"fastapi/fastapi": {
-    "MAP_REPO_TO_VERSION_PATHS": ["fastapi/__init__.py"],
-    "MAP_REPO_TO_VERSION_PATTERNS":  ["__version__ = ['\"](.*)['\"]", "VERSION = \\((.*)\\)"],
+"OpenInterpreter/open-interpreter": {
+    "MAP_REPO_TO_VERSION_PATHS": ["pyproject.toml"],
+    "MAP_REPO_TO_VERSION_PATTERNS": ["version\\s*=\\s*['\"]([0-9]+\\.[0-9]+\\.[0-9]+(?:-rc[0-9]+)?)['\"]"],
     "MAP_REPO_VERSION_TO_SPECS": {
             k: {
-            "python": "3.7",
+            "python": "3.11",
             "install": "pip install -e '.[all, dev, test]'",
-            "pip_packages": [
-                "'flask<2.3.0'"
-            ],
-            "test_cmd": "pytest -rA --tb=long", 
+            "test_cmd": "pytest -rA --tb=long",
         }
-        for k in ['0.55', '0.56', '0.87']
+        for k in ["0.1"]
     }
 },
 }
