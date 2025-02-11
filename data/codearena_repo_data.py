a527496b604cf5623be3241c83f8b03500a0d6a8
Gloria Geng <ggeng844@gmail.com>
2025-02-10 23:13:56 -0500

    onboarded scrapy 6542, 6519, 6475; 6540 fails 1 test, 6547 and 6472 behaving weird

--- data/codearena_repo_data.py
+++ data/codearena_repo_data.py
@@ -1,34 +1,21 @@
 {
-"fastapi/fastapi": {
-    "MAP_REPO_TO_VERSION_PATHS": ["fastapi/__init__.py"],
-    "MAP_REPO_TO_VERSION_PATTERNS":  ["__version__ = ['\"](.*)['\"]", "VERSION = \\((.*)\\)"],
-    "MAP_REPO_VERSION_TO_SPECS": {
+    "scrapy/scrapy": {
+        "MAP_REPO_TO_VERSION_PATHS": ["scrapy/VERSION"],
+        "MAP_REPO_TO_VERSION_PATTERNS": ["(\S*)"],
+        "MAP_REPO_VERSION_TO_SPECS": {
             k: {
-            "python": "3.7",
-            "install": "pip install -e '.[all, dev, test]'",
-            "pip_packages": [
-                "'flask<2.3.0'"
-            ],
-            "test_cmd": "pytest -rA --tb=long", 
-        }
-        for k in ['0.55', '0.56', '0.87']
-    }
-},
-"scrapy/scrapy": {
-    "MAP_REPO_TO_VERSION_PATHS": ["scrapy/VERSION"],
-    "MAP_REPO_TO_VERSION_PATTERNS":  ["(\S*)"],
-    "MAP_REPO_VERSION_TO_SPECS": {
-            k: {
-            "python": "3.9",
-            "install": "pip install -e '.'",
-            "pip_packages": [
-                "tox",
-                "testfixtures",
-                "pytest",
-            ],
-            "test_cmd": "pytest -rA --tb=long", 
-        }
-        for k in ['2.12', '2.11']
-    }
-},
+                "python": "3.9",
+                "install": "pip install -e '.'",
+                "pip_packages": [
+                    "tox",
+                    "testfixtures",
+                    "pytest",
+                    "sock",
+                    "testbed",
+                ],
+                "test_cmd": "pytest -rA --tb=long",
+            }
+            for k in ["2.12", "2.11"]
+        },
+    },
 }
