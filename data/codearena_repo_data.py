e67fd910d270bada4dc78288b01a5f4cc7f587fb
gc558 <gc558@cornell.edu>
2025-03-09 22:01:44 -0400

    Update Celery validation scripts and fix add_data.py duplication issue

--- data/codearena_repo_data.py
+++ data/codearena_repo_data.py
@@ -1,17 +1,34 @@
 {
-"fastapi/fastapi": {
-    "MAP_REPO_TO_VERSION_PATHS": ["fastapi/__init__.py"],
-    "MAP_REPO_TO_VERSION_PATTERNS":  ["__version__ = ['\"](.*)['\"]", "VERSION = \\((.*)\\)"],
+"celery/celery": {
+    "MAP_REPO_TO_VERSION_PATHS": ["celery/__init__.py"],
+    "MAP_REPO_TO_VERSION_PATTERNS": ["__version__ = ['\"](.*)['\"]"],
     "MAP_REPO_VERSION_TO_SPECS": {
             k: {
-            "python": "3.7",
-            "install": "pip install -e '.[all, dev, test]'",
+            "python": "3.9", 
+            "install": "pip install -U -r requirements/test.txt",
             "pip_packages": [
-                "'flask<2.3.0'"
+                "pytest==8.3.4",
+                "pytest-xdist",
+                "pytest-timeout",
+                "pytest-subtests",
+                "redis",
+                "kombu",
+                "vine",
+                "amqp",
+                "case",
+                "billiard",
+                "nose",
+                "importlib-metadata",
+                "importlib-resources",
+                "pydantic",
+                "dnspython",
+                "pymongo>=3.12.0",
+                "django-celery",
+                "elasticsearch"
             ],
-            "test_cmd": "pytest -rA --tb=long", 
+            "test_cmd": "pytest -rA --tb=long"
         }
-        for k in ['0.55', '0.56', '0.87']
+        for k in ["4.2", "4.3", "4.4","5.0", "5.1", "5.2", "5.3", "5.4", "5.5"]
     }
-},
+}
 }
