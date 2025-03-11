60b77d590180e8dd7b3224d6f45b4bd616c0e704
Gloria Geng <ggeng844@gmail.com>
2025-03-10 21:50:34 -0400

    I think it's all merged??

--- data/codearena_repo_data.py
+++ data/codearena_repo_data.py
@@ -29,4 +29,21 @@
         for k in ['2021.12', '2019.11']
     }
 },
+"scrapy/scrapy": {
+    "MAP_REPO_TO_VERSION_PATHS": ["scrapy/VERSION"],
+    "MAP_REPO_TO_VERSION_PATTERNS":  ["(\S*)"],
+    "MAP_REPO_VERSION_TO_SPECS": {
+            k: {
+            "python": "3.9",
+            "install": "pip install -e '.'",
+            "pip_packages": [
+                "tox",
+                "testfixtures",
+                "pytest",
+            ],
+            "test_cmd": "pytest -rA --tb=long", 
+        }
+        for k in ['2.12', '2.11']
+    }
+},
 }
