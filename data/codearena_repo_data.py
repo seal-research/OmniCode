be83fbdce061e60b8059213aba5644e158cc6987
Gloria Geng <ggeng844@gmail.com>
2025-03-18 16:40:17 -0400

    finished onboarding 20 instances

--- data/codearena_repo_data.py
+++ data/codearena_repo_data.py
@@ -37,13 +37,21 @@
             "python": "3.9",
             "install": "pip install -e '.'",
             "pip_packages": [
-                "tox",
-                "testfixtures",
-                "pytest",
-            ],
+                    "tox",
+                    "testfixtures",
+                    "pytest",
+                    "sock",
+                    "pillow",
+                    "botocore",
+                    "pexpect",
+                    "uvloop",
+                    "zstd",
+                    "brotli",
+                    "twisted",
+                ],
             "test_cmd": "pytest -rA --tb=long", 
         }
-        for k in ['2.12', '2.11']
+        for k in ['2.12', '2.11', '2.6', '2.5']
     }
 },
 }
