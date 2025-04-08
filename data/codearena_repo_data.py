66a3778d30b91ad86bf28a9b576b08270ba16733
Gloria Geng <ggeng844@gmail.com>
2025-04-08 17:51:13 -0400

    add gloria_scrapy_instances and modify codearena_repo_data

--- data/codearena_repo_data.py
+++ data/codearena_repo_data.py
@@ -68,4 +68,29 @@
         for k in [None]
     }
 },
+"scrapy/scrapy": {
+    "MAP_REPO_TO_VERSION_PATHS": ["scrapy/VERSION"],
+    "MAP_REPO_TO_VERSION_PATTERNS":  ["(\S*)"],
+    "MAP_REPO_VERSION_TO_SPECS": {
+            k: {
+            "python": "3.9",
+            "install": "pip install -e '.'",
+            "pip_packages": [
+                    "tox",
+                    "testfixtures",
+                    "pytest",
+                    "sock",
+                    "pillow",
+                    "botocore",
+                    "pexpect",
+                    "uvloop",
+                    "zstd",
+                    "brotli",
+                    "twisted",
+                ],
+            "test_cmd": "pytest -rA --tb=long", 
+        }
+        for k in ['2.12', '2.11', '2.6', '2.5']
+    }
+},
 }
