f0fa4564c309e236c0cc8eec90f5c63ad944558e
Carter Larsen <cjl259@cornell.edu>
2025-04-09 16:26:38 -0400

    Add instances and repo data

--- data/codearena_repo_data.py
+++ data/codearena_repo_data.py
@@ -68,4 +68,41 @@
         for k in [None]
     }
 },
+"scrapy/scrapy": {
+        "MAP_REPO_TO_VERSION_PATHS": ["scrapy/VERSION"],
+        "MAP_REPO_TO_VERSION_PATTERNS": ["(\S*)"],
+        "MAP_REPO_VERSION_TO_SPECS": {
+            **{
+                k: {
+                    "python": "3.9",
+                    "install": "pip install -e '.'",
+                    "pip_packages": [
+                        "tox",
+                        "testfixtures",
+                        "pytest",
+                        "pexpect",
+                        "botocore",
+                        "boto3",
+                        "google-cloud-storage",
+                    ],
+                    "test_cmd": "pytest -rA --tb=long",
+                }
+                for k in ["2.6", "2.7", "2.8", "2.9"]
+            },
+            **{
+                k: {
+                    "python": "3.9",
+                    "install": "pip install -e '.'",
+                    "pip_packages": [
+                        "tox",
+                        "testfixtures",
+                        "pytest",
+                        "pexpect",
+                    ],
+                    "test_cmd": "pytest -rA --tb=long",
+                }
+                for k in ["2.12", "2.11", "2.10"]
+            }
+        },
+    },
 }
