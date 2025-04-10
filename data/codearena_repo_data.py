981b811136a899a6c4a65390f48e5e9e8266850b
Ronit Pattanayak <rpatt@umich.edu>
2025-04-10 15:49:28 -0400

    Adding Camel AI instances

--- data/codearena_repo_data.py
+++ data/codearena_repo_data.py
@@ -44,5 +44,18 @@
     },
     # "MAP_REPO_TO_REQS_PATHS": ["requirements.txt"],
 },
+"camel-ai/camel": {
+    "MAP_REPO_TO_VERSION_PATHS": ["camel/__init__.py"],
+    "MAP_REPO_TO_VERSION_PATTERNS": ["__version__ = ['\"](.*)['\"]", "VERSION = \\((.*)\\)"],
+    "MAP_REPO_VERSION_TO_SPECS": {
+            k: {
+            "python": "3.10",
+            "install": "pip install -e '.[all, dev, test]'",
+            "pip_packages" : ["'gradio'"],
+            "test_cmd": "pytest -rA --tb=long",
+        }
+        for k in ["0.2"]
+    }
+}
 
 }
