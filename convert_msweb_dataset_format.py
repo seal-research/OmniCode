24e977a692897fc38da155b710cbd5782127b29d
Gloria Geng <ggeng844@gmail.com>
2025-04-24 12:06:04 -0400

    amend script to write json to file in components

--- convert_msweb_dataset_format.py
+++ convert_msweb_dataset_format.py
@@ -21,13 +21,10 @@ if __name__ == "__main__":
 
             new_json = {"repo":f"{instance["org"]}/{instance["repo"]}", "pull_number": instance["number"], "instance_id":instance["instance_id"], "issue_numbers":[], "base_commit":instance["base"]["sha"], "patch":instance["fix_patch"], "test_patch":instance["test_patch"], "problem_statement":f"{instance["title"]}\n{instance["body"]}", "hints_text":"", "created_at":"", "version":"", "PASS_TO_PASS":[], "FAIL_TO_PASS":[]}
 
-            # new_json = json.dumps(new_json)
 
             new_format.append(new_json)
 
-    output_path = Path("msweb_codearena_formatted.jsonl")
+    output_path = Path("data/components/msweb_codearena_formatted.json")
     with open(output_path, "w") as f:
-        for l in new_format:
-            json.dump(l, f)
-            f.write("\n")
+        json.dump(new_format, f)
             
