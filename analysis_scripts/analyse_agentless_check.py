43f4dfc82e092d5ae2f7ecaadcaba9e619cc147f
Atharv Sonwane <atharvs.twm@gmail.com>
2025-05-07 16:36:22 -0400

    fixes to test gen eval

--- analysis_scripts/analyse_agentless_check.py
+++ analysis_scripts/analyse_agentless_check.py
@@ -3,8 +3,14 @@ import json
 
 def analyse(
     results_dir: str | Path,
+    input_data: str | Path,
+    output_data: str | Path,
+    patches_dir: str | Path
 ):
-    results_dir = Path(results_dir)
+    results_dir, patches_dir = Path(results_dir), Path(patches_dir)
+
+    original_data = json.loads(Path(input_data).read_text())
+    with_patches_data = []
     
     skipped, present, bp_present, bp_missing = [], [], [], []
 
@@ -20,6 +26,18 @@ def analyse(
             continue
 
         resolved_dict = {}
+
+
+        instances = [i for i in original_data if i['instance_id'] == instance_id]
+        if len(instances) == 0:
+            continue
+
+        original_instance = instances[0]
+        if "bad_patches" not in original_instance or len(original_instance["bad_patches"]) == 0:
+            original_instance["bad_patches"] = []
+            max_id = 0
+        else:
+            max_id = max([bp['idx'] for bp in original_instance['bad_patches']])
 
         for pdir in patch_dirs:
 
@@ -55,13 +73,22 @@ def analyse(
 
         if any(r == False for r in resolved_dict.values()):
             bp_present.append(instance_id)
+            max_id += 1
+            patch = json.loads((patches_dir / f"{pdir.name}.jsonl").read_text())["model_patch"]
+            original_instance['bad_patches'].append({"idx": max_id, "patch": patch})
+
         else:
             bp_missing.append(instance_id)
 
-
+        with_patches_data.append(original_instance)
+    
+    
 
     print(f"Skipped: {skipped}")
     print(f"{len(present)=}, {len(skipped)=}, {len(bp_present)=}, {len(bp_missing)=}")
+
+    Path(output_data).write_text(json.dumps(with_patches_data, indent=2))
+    print(f"Wrote instances with patches to {output_data}")
 
     # all_ids = set([i for i in Path("/Users/ays57/Documents/opus/seds/codearena/data/instance_ids.txt").read_text().splitlines()])
     # remaining = all_ids - set(present)
