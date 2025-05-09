8bf7b04dfed5ac4124574cf8bae26f60184dee58
Yanke Mao <78478237+YankeMao@users.noreply.github.com>
2025-05-09 20:25:43 +0800

    Update codearena_repo_data.py for 13 celery instances

--- data/codearena_repo_data.py
+++ data/codearena_repo_data.py
@@ -152,37 +152,91 @@
     "MAP_REPO_TO_VERSION_PATHS": ["celery/__init__.py"],
     "MAP_REPO_TO_VERSION_PATTERNS": ["__version__ = ['\"](.*)['\"]"],
     "MAP_REPO_VERSION_TO_SPECS": {
+
+        ** {
             k: {
-            "python": "3.9", 
-            "install": "pip install -U -r requirements/test.txt",
-            "pip_packages": [
-                "pytest==8.3.4",
-                "pytest-xdist",
-                "pytest-timeout",
-                "pytest-subtests",
-                "redis",
-                "kombu",
-                "vine",
-                "amqp",
-                "case",
-                "billiard",
-                "nose",
-                "importlib-metadata",
-                "importlib-resources",
-                "pydantic",
-                "dnspython",
-                "pymongo==4.10.1",
-                "django-celery",
-                "elasticsearch",
-                "sqlalchemy==2.0.38",
-                "python-memcached==1.62",
-                "tblib",
-                "pytz",
-                "ephem==4.2.0"
-            ],
-            "test_cmd": "pytest -rA --tb=long"
+                "python": "3.9",
+                "install": "pip install -U -r requirements/test.txt",
+                "pip_packages": [
+                    "pytest==8.3.4",
+                    "pytest-xdist",
+                    "pytest-timeout",
+                    "pytest-subtests",
+                    "redis",
+                    "kombu",
+                    "vine",
+                    "amqp",
+                    "case",
+                    "billiard",
+                    "nose",
+                    "importlib-metadata",
+                    "importlib-resources",
+                    "pydantic",
+                    "dnspython",
+                    "pymongo==4.10.1",
+                    "django-celery",
+                    "elasticsearch",
+                    "sqlalchemy==2.0.38",
+                    "python-memcached==1.62",
+                    "tblib",
+                    "pytz",
+                    "ephem==4.2.0"
+                ],
+                "test_cmd": "pytest -rA --tb=long"
+            }
+            for k in ["5.1", "5.2", "5.3", "5.4", "5.5"]
+        },
+        **{
+            k: {
+                "python": "3.9",
+                "install": "",
+                "pip_packages": [
+                    "pytest==7.1.3",
+                    "pytest-xdist==2.5.0",
+                    "pytest-timeout",
+                    "pytest-subtests==0.10.0",
+                    "pytest-cov==2.12.1",
+                    "case",
+                    "kombu",
+                    "billiard",
+                    "pytz",
+                    "click",
+                    "flaky",
+                    "vine==1.3.0",
+                    "redis",
+                ],
+                "test_cmd": "pytest -rA --tb=long"
+            }
+            for k in ["5.0"]
+        },
+        ** {
+            k: {
+                "python": "3.9",
+                "install": "",
+                "pip_packages": [
+                    "pytest==7.1.3",
+                    "pytest-xdist==2.5.0",
+                    "pytest-timeout",
+                    "pytest-subtests==0.10.0",
+                    "pytest-cov==2.12.1",
+                    "case",
+                    "kombu",
+                    "billiard==3.6.4.0",
+                    "pytz",
+                    "click",
+                    "flaky",
+                    "vine==1.3.0",
+                    "redis",
+                    "boto3",
+                    "cryptography",
+                    "sqlalchemy",
+                    "future",
+                    "tblib",
+                ],
+                "test_cmd": "pytest -rA --tb=long"
+              }
+              for k in ["4.2", "4.3", "4.4"]
         }
-        for k in ["4.2", "4.3", "4.4","5.0", "5.1", "5.2", "5.3", "5.4", "5.5"]
     }
-}
+  }
 }
