1fe8cd85d070596851ecc83e97de4d53effab7c7
James Tu <jamestu6301@gmail.com>
2025-07-13 17:00:12 +0800

    update: install swe-rex with pipx modified base images for mswebench instance to install swe-rex with pipx

--- multiswebench/multi_swe_bench/harness/repos/java/springprojects/springboot.py
+++ multiswebench/multi_swe_bench/harness/repos/java/springprojects/springboot.py
@@ -62,9 +62,11 @@ RUN curl -s https://repos.azul.com/azul-repo.key | gpg --dearmor -o /usr/share/k
     && echo "deb [signed-by=/usr/share/keyrings/azul.gpg] https://repos.azul.com/zulu/deb stable main" | tee /etc/apt/sources.list.d/zulu.list
 RUN apt update && apt install -y zulu21-jdk
 
-# install swe-agent/swe-rex dependencies:
-RUN apt-get update && apt-get install -y python3 python3-pip
-RUN pip3 install swe-rex
+# install swe-rex for sweagent:
+RUN apt-get update && apt-get install -y python3 python3-pip python3-venv
+RUN pip3 install pipx && \
+    pipx ensurepath && \
+    pipx install swe-rex
 
 {code}
 
