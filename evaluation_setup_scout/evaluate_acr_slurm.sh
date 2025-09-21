fc1bbce4d3ea8228af998601df54379db4f0a872
ClaasBeger <claas_baller@yahoo.com>
2025-09-20 20:24:23 -0600

    Fixed command format

--- evaluation_setup_scout/evaluate_acr_slurm.sh
+++ evaluation_setup_scout/evaluate_acr_slurm.sh
@@ -33,7 +33,7 @@ while IFS= read -r ID || [[ -n "${ID}" ]]; do
            --time="${TIME_LIMIT}" \
            --output="${LOG_DIR}/%x_%j.out" \
            --error="${LOG_DIR}/%x_%j.err" \
-           --wrap="(cd /home/cbb89/codearena/codearena/OmniCode && python codearena.py --Bugfixing \
+           --wrap="(cd /home/cbb89/codearena/codearena/OmniCode && python codearena.py --BugFixing \
                     --predictions_path gold \
                     --run_id ${JOB_NAME} \
                     --max_workers 1 \
@@ -65,7 +65,7 @@ while IFS= read -r ID || [[ -n "${ID}" ]]; do
            --time="${TIME_LIMIT}" \
            --output="${LOG_DIR}/%x_%j.out" \
            --error="${LOG_DIR}/%x_%j.err" \
-           --wrap="(cd /home/cbb89/codearena/codearena/OmniCode && python codearena.py --Codereview \
+           --wrap="(cd /home/cbb89/codearena/codearena/OmniCode && python codearena.py --CodeReview \
                     --predictions_path gold \
                     --run_id ${JOB_NAME} \
                     --max_workers 1 \
@@ -97,7 +97,7 @@ while IFS= read -r ID || [[ -n "${ID}" ]]; do
            --time="${TIME_LIMIT}" \
            --output="${LOG_DIR}/%x_%j.out" \
            --error="${LOG_DIR}/%x_%j.err" \
-           --wrap="(cd /home/cbb89/codearena/codearena/OmniCode && python codearena.py --Stylereview \
+           --wrap="(cd /home/cbb89/codearena/codearena/OmniCode && python codearena.py --StyleReview \
                     --predictions_path gold \
                     --run_id ${JOB_NAME} \
                     --max_workers 1 \
@@ -129,7 +129,7 @@ while IFS= read -r ID || [[ -n "${ID}" ]]; do
            --time="${TIME_LIMIT}" \
            --output="${LOG_DIR}/%x_%j.out" \
            --error="${LOG_DIR}/%x_%j.err" \
-           --wrap="(cd /home/cbb89/codearena/codearena/OmniCode && python codearena.py --Testgen \
+           --wrap="(cd /home/cbb89/codearena/codearena/OmniCode && python codearena.py --TestGeneration \
                     --predictions_path gold \
                     --run_id ${JOB_NAME} \
                     --max_workers 1 \
