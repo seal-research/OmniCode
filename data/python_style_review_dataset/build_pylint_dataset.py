6faeb98947e73d5b430176b2a16ee2995544fc83
James Tu <jamestu6301@gmail.com>
2025-09-22 09:58:45 +0800

    update: script for building style review dataset

--- data/python_style_review_dataset/build_pylint_dataset.py
+++ data/python_style_review_dataset/build_pylint_dataset.py
@@ -29,33 +29,35 @@ for key, (url, pr) in repos.items():
     repo_name = key.split('_')[-1]
 
     ### Phase 1: collecting and running pylint on dataset
-    # try:
-    #     subprocess.run([
-    #         "python", SCRIPT,
-    #         "--repo-url", url,
-    #         "--pr", str(pr),
-    #         "--out", out_path,
-    #         "--work-dir", workdir
-    #     ], check=False)
-    # finally:
-    #     # cleanup: remove workdir + cloned repo
-    #     print(f"[+] Completed pylint review for {key}_{pr}")
-    #     shutil.rmtree(f'./repo', ignore_errors=True)
+    try:
+        subprocess.run([
+            "python", SCRIPT,
+            "--repo-url", url,
+            "--pr", str(pr),
+            "--out", out_path,
+            "--work-dir", workdir
+        ], check=False)
+    finally:
+        # cleanup: remove workdir + cloned repo
+        print(f"[+] Completed pylint review for {key}_{pr}")
+        shutil.rmtree(f'./repo', ignore_errors=True)
+
     ### Phase 2: filtering dataset
-    # try:
-    #     subprocess.run([
-    #         "python", 'filter_pylint_results.py',
-    #         "--instance-id", f'{key}-{pr}',
-    #         "--input", f'pylint_results/full_pylint_run_data/full_{key.split('__')[-1]}_pylint.json'
-    #     ], check=False)
-    # finally:
-    #     # cleanup: remove workdir + cloned repo
-    #     print(f"[+] Completed pylint review for {key}_{pr}")
-    #     shutil.rmtree(f'./repo', ignore_errors=True)
+    try:
+        subprocess.run([
+            "python", 'filter_pylint_results.py',
+            "--instance-id", f'{key}-{pr}',
+            "--input", f'pylint_results/full_pylint_run_data/full_{key.split('__')[-1]}_pylint.json'
+        ], check=False)
+    finally:
+        # cleanup: remove workdir + cloned repo
+        print(f"[+] Completed pylint review for {key}_{pr}")
+        shutil.rmtree(f'./repo', ignore_errors=True)
+
     ### Phase 3: splitting dataset
     try:
         subprocess.run([
-            "python", 'python_style_review_dataset_splitter.py',
+            "python", 'python_style_review_dataset_generator.py',
             "--instance-id", f'{key}-{pr}',
             "--results", f'pylint_results/{key}-{pr}.json',
             "--output", f'python_style_review_dataset/{key}-{pr}_review_instances.json'
